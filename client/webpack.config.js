53830b0e13ef2a10b13c4e238019124a4994aa02
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-05-02 23:49:40 -0400

    trying to fix debugs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,15 +3,12 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
@@ -32,7 +29,7 @@ module.exports = () => {
         inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
-        description: 'WriteMate: A Progressive Web App Text Editor with Offline Capabilities',
+        description: 'Unless we remember, we cannot understand',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -42,26 +39,35 @@ module.exports = () => {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
+
+          }
+        ]
+      })
     ],
+
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: [
+            'style-loader',
+            'css-loader'
+          ],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
+              presets: [
+                '@babel/preset-env'
+              ],
               plugins: [
                 '@babel/plugin-proposal-object-rest-spread',
-                '@babel/transform-runtime',
+                '@babel/transform-runtime'
               ],
             },
           },
